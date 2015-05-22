e7f0f1ea860e6d6bd3a679f1bd481e20ab10b2c2
r3dh4r7 <r3dh4r7@CYPHERT>
2015-05-22 21:25:16 +0200

    Uploaded

---
+++ Assignment #1/Answer 1.cpp
@@ -0,0 +1,30 @@
+/*
+Name: Onwumere Kate Kelechi
+Department: Computer Science
+Reg. No: 47138136EJ
+
+Assignment #1 answer 1
+*/
+ 
+#include<iostream>
+using namespace std;
+
+int main()
+{int integer;
+
+cout<<"please enter an integer"
+<<endl;
+cin>>integer;
+
+if(integer%2==0)
+
+cout<<integer<<" The number you inputed is an even nuumber"
+<<endl;
+else
+
+cout<<integer<<" The number you inputed is an odd number"
+<<endl;
+
+return 0;
+}
+
---
+++ Assignment #1/Answer 5.cpp
@@ -0,0 +1,75 @@
+/*
+Name: Onwumere Kate Kelechi
+Department: Computer Science
+Reg. No: 47138136EJ
+
+Assignment #1 answer 5
+*/
+
+#include<iostream>
+using namespace std;
+
+
+int main()
+
+{
+    int num;
+	
+	num=0;
+	cout<<"\nnumber\tsquare\tcube\n"<<num<<"\t"<<num*num<<"\t"<<num*num*num<<"\n";
+	
+	num=num+1;
+	cout<<num<<"\t"<<num*num<<"\t"<<
+	num*num*num<<"\n";
+	
+	
+	num=num+1;
+	cout<<num<<"\t"<<num*num<<"\t"<<
+	num*num*num<<"\n";
+	
+	
+	num=num+1;
+	cout<<num<<"\t"<<num*num<<"\t"<<
+	num*num*num<<"\n";
+	
+	
+	num=num+1;
+	cout<<num<<"\t"<<num*num<<"\t"<<
+	num*num*num<<"\n";
+	
+	
+	num=num+1;
+	cout<<num<<"\t"<<num*num<<"\t"<<
+	num*num*num<<"\n";
+	
+	
+	num=num+1;
+	cout<<num<<"\t"<<num*num<<"\t"<<
+	num*num*num<<"\n";
+	
+	
+	num=num+1;
+	cout<<num<<"\t"<<num*num<<"\t"<<
+	num*num*num<<"\n";
+	
+	
+	num=num+1;
+	cout<<num<<"\t"<<num*num<<"\t"<<
+	num*num*num<<"\n";
+	
+	
+	num=num+1;
+	cout<<num<<"\t"<<num*num<<"\t"<<
+	num*num*num<<"\n";
+	
+	
+	num=num+1;
+	cout<<num<<"\t"<<num*num<<"\t"<<
+	num*num*num<<"\n";
+	
+	
+	
+	return 0;
+}
+
+
